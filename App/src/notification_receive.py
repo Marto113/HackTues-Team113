17ca86c6015947527a0ce7b436bf9684d7cb4d8e
ToxicSDev <kaloqn.dimitrov05@abv.bg>
2023-03-10 14:33:21 +0200

    Updated notification_receive

--- App/src/notification_receive.py
+++ App/src/notification_receive.py
@@ -1,19 +1,14 @@
 import asyncio
 import telegram
+from dotenv import dotenv_values
 
-async def send(token, chat_id):
-    bot = telegram.Bot(token)
-    message = "ALARM IS GOING OFF"
-    await bot.send_message(chat_id, message)
+async def send_alarm(message):
+    config = dotenv_values("../.env")
+    token = config['telegram_token']
+    chat_id = config['telegram_chat_id']
 
-async def main():
-    # bot token using BotFather
-    token = '6063360147:AAG1O9B9dClWpKNb8IO1HW3672Xc3pMvnU0'
-
-    # group to send the message to
-    chat_id = '-977069027'
-    await send(token, chat_id)
+    async with telegram.Bot(token) as bot:
+        await bot.send_message(chat_id, message)
 
 if __name__ == '__main__':
-    asyncio.run(main())
-    
+    asyncio.run(send_alarm("The Alarm has been triggered!"))
