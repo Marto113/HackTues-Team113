41681be2106f3ae96592b36e430509871460ed7f
ToxicSDev <kaloqn.dimitrov05@abv.bg>
2023-03-10 23:05:12 +0200

    Update server.py

--- App/src/server.py
+++ App/src/server.py
@@ -11,7 +11,6 @@ import binascii
 
 app = Flask(__name__, static_url_path="/static")
 config = dotenv_values("../.env")
-app.config['SECRET_KEY'] = config['secret_key']
 app.config['UPLOAD_FOLDER'] = '../whitelist'
 app.config['EVIDENCE_FOLDER'] = '../evidence'
 app.config['ALLOWED_EXTENSIONS'] = {'jpg', 'jpeg', 'png'}
