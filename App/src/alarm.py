e69652dedf1a2e4a455cc2c49434298b7a1ad7bc
ToxicSDev <kaloqn.dimitrov05@abv.bg>
2023-03-10 15:38:23 +0200

    Telegram api to send image of intruder

--- App/src/alarm.py
+++ App/src/alarm.py
@@ -2,13 +2,15 @@ import asyncio
 import telegram
 from dotenv import dotenv_values
 
-async def send_alarm(message):
+async def telegram_alert(message, image):
     config = dotenv_values("../.env")
     token = config['telegram_token']
     chat_id = config['telegram_chat_id']
 
     async with telegram.Bot(token) as bot:
         await bot.send_message(chat_id, message)
+        with open(image, 'rb') as f:
+            await bot.send_photo(chat_id, f)
 
 if __name__ == '__main__':
-    asyncio.run(send_alarm("The Alarm has been triggered!"))
+    asyncio.run(telegram_alert("The Alarm has been triggered!", "D:\Programing\HackTues-Team113\App\src\static\images\icon.png"))
